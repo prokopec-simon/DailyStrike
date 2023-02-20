690679fe77ec9847bb360b6cafd213e0202db9f3
Šimon Prokopec <prokopec.simon@nvsp.cz>
2023-02-20 21:40:54 +0100

    Predicting block separation, number input

--- src/components/DailyMatchup.tsx
+++ src/components/DailyMatchup.tsx
@@ -1,42 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
 import { Match } from "@prisma/client";
 import { useSession } from "next-auth/react";
-import { useCallback, useState } from "react";
+import { useState } from "react";
 import CountdownTimer from "./CountdownTimer";
-
-const PredictingBlock: React.FC<{
-  match: Match;
-  selectedTeam: string;
-}> = ({ match, selectedTeam }) => {
-  const [input, setInput] = useState("");
-
-  const teamAOdds = Number(match.teamA_odds);
-  const teamBOdds = Number(match.teamB_odds);
-  const winProbability =
-    selectedTeam === match.teamA_name
-      ? teamAOdds * Number(input)
-      : teamBOdds * Number(input);
-
-  const setInputCallback = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-      setInput(e.target.value);
-    },
-    []
-  );
-
-  return (
-    <div>
-      <div>Expected win: {winProbability.toFixed(3)}</div>
-      <input
-        className="h-8 w-2/5 rounded bg-zinc-600 px-2 text-right"
-        onInput={setInputCallback}
-      />
-      <button className="mt-1 mr-4 w-2/5 rounded bg-orange-500 p-1 px-4 text-center text-sm text-white">
-        Predict
-      </button>
-    </div>
-  );
-};
+import { PredictingBlock } from "./PredictingBlock";
 
 const DailyMatchComponent: React.FC<{ match: Match }> = (props) => {
   const [selectedTeam, setSelectedTeam] = useState<string | null>(null);
