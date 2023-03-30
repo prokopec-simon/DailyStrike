99d6853c25f3607be9a8f1633d01ed570c09468b
Šimon Prokopec <prokopec.simon@nvsp.cz>
2023-03-30 11:26:23 +0200

    attempts to fix requests

--- src/server/common/get-server-auth-session.ts
+++ src/server/common/get-server-auth-session.ts
@@ -1,11 +1,14 @@
 // Wrapper for unstable_getServerSession https://next-auth.js.org/configuration/nextjs
 
-import type { GetServerSidePropsContext } from "next";
+import type { GetServerSidePropsContext, NextApiRequest } from "next";
+import { unstable_getServerSession } from "next-auth";
+import getOptions from "../../pages/api/auth/authOptions";
 
 // Next API route example - /pages/api/restricted.ts
 export const getServerAuthSession = async (ctx: {
   req: GetServerSidePropsContext["req"];
   res: GetServerSidePropsContext["res"];
 }) => {
-  //
+  const options = await getOptions(ctx.req as NextApiRequest);
+  return await unstable_getServerSession(ctx.req, ctx.res, options);
 };
