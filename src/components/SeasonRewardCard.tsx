17a9639b2c53eea5f3a3833416078d10320cd285
Šimon Prokopec <prokopec.simon@nvsp.cz>
2023-07-25 10:26:43 +0200

    Season reward card placement, table formatting

--- src/components/SeasonRewardCard.tsx
+++ src/components/SeasonRewardCard.tsx
@@ -1,28 +1,49 @@
 import Image from "next/image";
 import { SeasonReward } from "@prisma/client";
+import { Tooltip } from "antd";
 
-const getOrdinalNum = (number: number) => {
+const getOrdinalNum = (place: number) => {
   const suffix =
-    (number >= 4 && number <= 20) || (number >= 24 && number <= 30)
+    (place >= 4 && place <= 20) || (place >= 24 && place <= 30)
       ? "th"
-      : ["st", "nd", "rd"][(number % 10) - 1];
+      : ["st", "nd", "rd"][(place % 10) - 1] || "th";
   return suffix;
 };
 
 const SeasonRewardCard: React.FC<{ seasonReward: SeasonReward }> = (props) => {
+  const { seasonReward } = props;
+  const ladderPlaceStart = seasonReward.ladderPlaceStart || "-";
+  const ladderPlaceEnd = seasonReward.ladderPlaceEnd || "-";
+  const ladderStartSuffix = getOrdinalNum(seasonReward.ladderPlaceStart);
+  const ladderEndSuffix = getOrdinalNum(seasonReward.ladderPlaceEnd);
+
   return (
-    <div className="rounded-md bg-zinc-600 bg-opacity-50 px-2">
-      <div className="absolute">
-        {props.seasonReward.ladderPlaceStart +
-          (getOrdinalNum(props.seasonReward.ladderPlaceStart) ?? "")}
-      </div>
-      <Image
-        src={props.seasonReward.itemImageUrl ?? ""}
-        alt="Item reward item"
-        width={100}
-        height={100}
-      />
-    </div>
+    <Tooltip placement="top" title={seasonReward.itemName}>
+      <div className="rounded-md bg-zinc-600 bg-opacity-50 px-2">
+        <div className="absolute">
+          {ladderPlaceStart === ladderPlaceEnd ? (
+            <>
+              {ladderPlaceStart}
+              {ladderStartSuffix}
+            </>
+          ) : (
+            <>
+              {ladderPlaceStart}
+              {ladderStartSuffix}
+              {" - "}
+              {ladderPlaceEnd}
+              {ladderEndSuffix}
+            </>
+          )}
+        </div>
+        <Image
+          src={seasonReward.itemImageUrl || ""}
+          alt="Item reward item"
+          width={100}
+          height={100}
+        />
+      </div>{" "}
+    </Tooltip>
   );
 };
 
