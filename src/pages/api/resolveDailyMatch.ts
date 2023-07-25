e31a44745bcb5356c486e97684cbee3da7dca84c
Šimon Prokopec <prokopec.simon@nvsp.cz>
2023-07-25 12:12:24 +0200

    Cleaned up match resolving endpoint

--- src/pages/api/resolveDailyMatch.ts
+++ src/pages/api/resolveDailyMatch.ts
@@ -2,59 +2,68 @@ import { NextApiResponse } from "next";
 import { prisma } from "../../server/db/client";
 import { Decimal } from "@prisma/client/runtime";
 import { AxiomAPIRequest, log, withAxiom } from "next-axiom";
+import { UserMatchPrediction } from "@prisma/client";
 
 const VALID_API_KEY = process.env.DAILYSTRIKE_PRIVATE_KEY;
 
 async function handler(req: AxiomAPIRequest, res: NextApiResponse) {
   req.log.info("Received attempt to resolve daily matches.");
+
   if (req.method !== "POST") {
-    res.status(405).json({ message: "Incorrect HTTP method" });
+    return res.status(405).json({ message: "Incorrect HTTP method" });
   }
 
   const apiKey = req.headers["dailystrike-key"] || req.query.apiKey;
 
-  if (apiKey === VALID_API_KEY) {
-    req.log.info("All conditions to resolve match fulfilled.");
+  if (apiKey !== VALID_API_KEY) {
+    req.log.flush();
+    return res.status(401).json({ error: "Unauthorized" });
+  }
 
-    const matchToResolve = await prisma?.match.findFirstOrThrow({
+  req.log.info("All conditions to resolve match fulfilled.");
+
+  try {
+    const matchToResolve = await prisma.match.findFirstOrThrow({
       orderBy: { dateAndTime: "desc" },
     });
 
     req.log.info("Resolving match", matchToResolve);
 
-    if (matchToResolve.winner == null) {
+    if (matchToResolve.winner === null) {
       req.log.info(
         "Incorrectly called match resolving, match is not finished."
       );
       req.log.flush();
-      res.status(400).json({ error: "Match shouldn't have been resolved." });
+      return res
+        .status(400)
+        .json({ error: "Match shouldn't have been resolved." });
     }
 
-    const predictionsToResolve = await prisma?.userMatchPrediction.findMany({
+    const predictionsToResolve = await prisma.userMatchPrediction.findMany({
       where: { matchId: matchToResolve.id },
     });
 
     req.log.info(`Resolving ${predictionsToResolve.length} predictions.`);
-    predictionsToResolve.forEach(async (prediction: any) => {
+
+    for (const prediction of predictionsToResolve) {
       const foundUser = await prisma.user.findFirstOrThrow({
         where: { id: prediction.userId },
       });
 
-      let balanceIncrease =
+      const balanceIncrease =
         Number(prediction.predictionOdds) * Number(prediction.predictionAmount);
+      let balanceChange = 0;
 
       if (matchToResolve.winner === prediction.pickedTeam) {
+        balanceChange = balanceIncrease;
         await prisma.user.update({
           where: { id: prediction.userId },
           data: {
-            balance: Decimal.add(
-              foundUser.balance,
-              new Decimal(balanceIncrease)
-            ),
+            balance: Decimal.add(foundUser.balance, new Decimal(balanceChange)),
           },
         });
       } else {
-        balanceIncrease = -balanceIncrease;
+        balanceChange = -balanceIncrease;
       }
 
       await prisma.userMatchPrediction.update({
@@ -65,17 +74,18 @@ async function handler(req: AxiomAPIRequest, res: NextApiResponse) {
           },
         },
         data: {
-          balanceChange: balanceIncrease,
-          balanceAfter: Decimal.add(foundUser.balance, balanceIncrease),
+          balanceChange: balanceChange,
+          balanceAfter: Decimal.add(foundUser.balance, balanceChange),
         },
       });
-    });
+    }
 
     req.log.flush();
-    res.status(200).json({ resultState: "Ok" });
-  } else {
+    return res.status(200).json({ resultState: "Ok" });
+  } catch (error: any) {
+    req.log.error("An error occurred while processing the request.", error);
     req.log.flush();
-    res.status(401).json({ error: "Unauthorized" });
+    return res.status(500).json({ error: "Internal Server Error" });
   }
 }
 
