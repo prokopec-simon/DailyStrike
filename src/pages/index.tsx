ca09d3327de137cfdebae9dcdbbcac2492bd41ca
Šimon Prokopec <prokopec.simon@nvsp.cz>
2023-11-05 14:45:40 +0100

    fixed recent result panel displaying before data fetched

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,12 +29,14 @@ const Home = () => {
       </div>
       <div className="flex w-full flex-col items-center justify-center">
         <div className="w-4/5 rounded-md bg-zinc-700 bg-opacity-20 pb-2 text-center md:mt-4 md:w-1/3 md:px-4">
-          <h3 className="pt-1 text-white md:-mb-5 md:pt-2 md:pb-1">
-            Recent results
-          </h3>
           {lastNMatches
             ? lastNMatches.map((match, index) => (
-                <HistoryMatch key={index} match={match}></HistoryMatch>
+                <>
+                  <h3 className="pt-1 text-white md:-mb-5 md:pt-2 md:pb-1">
+                    Recent results
+                  </h3>
+                  <HistoryMatch key={index} match={match}></HistoryMatch>
+                </>
               ))
             : null}
         </div>
